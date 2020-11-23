e51ee019ad6e58716894de7dd9d199f7dc420b83
anna lazarenkova <anna.lazarenkova@gmail.com>
2020-11-23 12:51:29 +0200

    anna lisas kolmas kord

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,9 @@ public class Main {
         System.out.println("hello. Anna lisas");
         System.out.println("Anna lisas osas 2");
         System.out.println("hello world");
+        for(int i = 0; i<10; i=i+2){
+            System.out.println("Nüüd teeme veel kord");
+        }
 	// write your code here
     }
 }
