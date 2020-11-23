2945603734faacd6d83a9a372482a70d1605323c
anna lazarenkova <anna.lazarenkova@gmail.com>
2020-11-23 11:51:55 +0200

    anna lisas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello. Anna lisas");
 	// write your code here
     }
 }
