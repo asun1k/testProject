a74379ddec98d5df9ad4a3817c22664e51b95e80
anna lazarenkova <anna.lazarenkova@gmail.com>
2020-11-23 13:27:41 +0200

    anna uus klass

---
+++ src/com/company/Request.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Request {
+
+    private String changes;
+
+    public String getChanges() {
+        return changes;
+    }
+
+    public void setChanges(String changes) {
+        this.changes = changes;
+    }
+}
