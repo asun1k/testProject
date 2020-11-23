4508c63f944b85c761ede4fe7e30c25fcb1af800
anna lazarenkova <anna.lazarenkova@gmail.com>
2020-11-23 12:03:11 +0200

    anna lisas teise osa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello. Anna lisas");
-	// write your code here
+        System.out.println("Anna lisas osas 2");
     }
 }
